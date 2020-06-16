43b10350107b3bac08eca67189de7bbc7e847f67
Rafael Brito <rafabrito.office@gmail.com>
2020-06-16 14:50:13 -0300

    URI - 1003 - Soma Simples

---
+++ Challenger BEGINNER/src/beginner/URI_1003.java
@@ -0,0 +1,20 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class URI_1003 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int soma = a + b;
+		
+		System.out.println("SOMA = " + soma);
+		
+		sc.close();
+	}
+
+}
